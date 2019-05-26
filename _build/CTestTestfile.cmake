801356695a8b7a28eb77a6e23eccffdbf817cb6a
dashavpav <dashavpavlov@gmail.com>
2019-05-26 19:11:51 +0300

    A

---
+++ _build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/dashavpav/dashavpav/workspace/projects/lab05
+# Build directory: /home/dashavpav/dashavpav/workspace/projects/lab05/_build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(check "/home/dashavpav/dashavpav/workspace/projects/lab05/_build/check")
+subdirs("third-party/gtest")
